c8aadc2e481a9b21a2ce8adb85e7761419385523
shivagayathri <shivagayathrik2000@gmail.com>
2025-10-04 13:51:05 +0530

    add order-service and mongo connection

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDER_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          host: 'localhost',
+          port: 6379,
+          retryAttempts: 5,
+          retryDelay: 3000,
+        },
+      },
+    ]),
+  ],
   providers: [OrderService],
-  controllers: [OrderController]
+  controllers: [OrderController],
 })
 export class OrderModule {}
